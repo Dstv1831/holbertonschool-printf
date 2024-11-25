02881b8af3e2f96ba95e2a6eeb8d556a31c84097
Nigel Feng <10083@holbertonstudents.com>
2024-11-25 07:31:04 +0000

    Update: Included function prototype in main.h and _printf.c

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - function that produces output according to a format.
+ * @format: pointer format pointing to char value.
+ *
+ * Return:
+ */
+int _printf(const char *format, ...)
+{
+  return ();
+}
--- main.h
+++ main.h
@@ -4,7 +4,7 @@
 #include <stdarg.h>
 
 int _putchar(char c);
-
+int _printf(const char *format, ...);
 
 #endif
 
