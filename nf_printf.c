09fe024a44bdb7c61e61aa5de8c4f5a2369994af
Nigel Feng <10083@holbertonstudents.com>
2024-11-25 08:13:30 +0000

    test

--- nf_printf.c
+++ nf_printf.c
@@ -1,5 +1,18 @@
+#include "main.h"
+#include <stdarg.h>
 
-asdfc ksdvnls
- cskjnkdsn
+int _printf(const char *format, ...)
+{
+  unsigned int i = 0;
+  
+  va_list(args);
+  va_start(args, format);
 
-Hello World...
+  while (format != NULL && format[i])
+    {
+      _putchar();
+      i++;
+    }
+  printf('\n');
+  va_end(args);
+}
