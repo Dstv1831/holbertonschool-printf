ca3d0a5659f2f14a623c938ed8a003fe82a4d04b
diu-ban <bongban2003@gmail.com>
2024-11-26 12:04:11 +1100

    task 0 1st attempt

---
+++ _printf.c
@@ -0,0 +1,70 @@
+#include "main.h"
+#include "stdarg.h"
+#include <stdlib.h>
+
+void print_string(va_list args);
+
+int _printf(const char *format, ...)
+{
+    va_list args;
+    unsigned int i = 0;
+    char *str;
+    // int len; 
+
+    va_start(args, format);
+
+    while (format && format[i])
+    {
+        if (format[i] == '%')
+        {
+            switch (format[i+1])
+            {
+                case 'c':
+                    char c = va_arg(args, int);
+                    _putchar(c);
+                    break;
+                case 's':
+                    print_string(args);
+                    break;
+                case '%':
+                    _putchar('%');
+                    break;
+                default:
+                    break;
+            }
+            i++;
+        }
+        else
+        {
+            _putchar(format[i]);
+        } 
+        i++;
+    }
+    _putchar('\n');
+    va_end(args);
+    return 0;
+}
+
+void print_string(va_list args)
+{
+    int i = 0;
+    char *str = va_arg(args, char *);
+
+    if (!str)
+    {
+        str = "(Nil)";
+        while (str[i] != '\0')
+        {
+            _putchar(str[i]);
+            i++;
+        }
+        return;
+    }
+
+    while (str[i])
+    {
+        _putchar(str[i]);
+        i++;
+    }
+    return;
+}
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,2 @@
+len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
