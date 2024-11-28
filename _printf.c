89f4c126c54a817d2688d2f0b2fd1a05039418df
DSTV <9871@holbertonstudents.com>
2024-11-28 03:41:41 +0000

    Last modification after man

--- _printf.c
+++ _printf.c
@@ -9,138 +9,164 @@ int print_string(va_list args);
 int print_int(va_list args);
 int print_number(int n);
 /**
- * _printf - prints an integer type argument ("int")
- * @format: list of all types of arguments passed
- * to the function
- * Return: len.
+ * _printf - prints a formated string into the standard output
+ * @format: formated string
+ *
+ * Return: amount of characters printed.
  */
 
 int _printf(const char *format, ...)
 {
-    va_list args;
-    unsigned int i = 0, count = 0;
-    char c;
+	va_list args;
+	unsigned int i = 0, count = 0;
+	char c;
 
-    if (!format)
-    {
-        return (-1);
-    }
+	if (!format)
+	{
+		return (-1);
+	}
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (format && format[i] != '\0')
-    {
-        if (format[i] == '%')
-        {
-            switch (format[i + 1])
-            {
-            case '\0':
-                return (-1);
-            case 'c':
-                c = va_arg(args, int);
-                _putchar(c);
-                count++;
-                i += 2;
-                break;
-            case 's':
-                count += print_string(args);
-                i += 2;
-                break;
-            case '%':
-                _putchar('%');
-                count++;
-                i += 2;
-                break;
-            case 'd':
-                count += print_int(args);
-                i += 2;
-                break;
-            case 'i':
-                count += print_int(args);
-                i += 2;
-                break;
-            default:
-                _putchar('%');
-                count++;
-                i++;
-                break;
-            }
-        }
-        else
-        {
-            _putchar(format[i]);
-            count++;
-            i++;
-        }
-    }
-    va_end(args);
-    return (count);
+	while (format && format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			switch (format[i + 1])
+			{
+				case '\0':
+					return (-1);
+				case 'c':
+					c = va_arg(args, int);
+					_putchar(c);
+					count++;
+					i += 2;
+					break;
+				case 's':
+					count += print_string(args);
+					i += 2;
+					break;
+				case '%':
+					_putchar('%');
+					count++;
+					i += 2;
+					break;
+				case 'd':
+					count += print_int(args);
+					i += 2;
+					break;
+				case 'i':
+					count += print_int(args);
+					i += 2;
+					break;
+				default:
+					_putchar('%');
+					count++;
+					i++;
+					break;
+			}
+		}
+
+		else
+
+		{
+			_putchar(format[i]);
+			count++;
+			i++;
+		}
+	}
+	va_end(args);
+	return (count);
 }
 
 /**
- * print_string - prints an integer type argument ("int")
- * @args: list of all types of arguments passed
- * to the function
- * Return: len.
+ * print_string - prints a string type into the standard output
+ *
+ * @args: list of all types of arguments passed to the function
+ *
+ * Return: amount of characters printed.
  */
 
 int print_string(va_list args)
 {
-    unsigned int i = 0, count = 0;
-    char *str = va_arg(args, char *);
+	unsigned int i = 0, count = 0;
+	char *str = va_arg(args, char *);
 
-    if (!str)
-    {
-        str = "(null)";
-    }
+	if (!str)
+	{
+		str = "(null)";
+	}
 
-    while (str[i])
-    {
-        _putchar(str[i]);
-        count++;
-        i++;
-    }
-    return (count);
+	while (str[i])
+	{
+		_putchar(str[i]);
+		count++;
+		i++;
+	}
+
+	return (count);
 }
+
+/**
+ * print_int - prints an integer into the standard output
+ *
+ * @args: list of all types of arguments passed to the function
+ *
+ * Return: amount of characters printed.
+ */
 
 int print_int(va_list args)
 {
-    int digit = va_arg(args, int);
-    return (print_number(digit));
+	int digit = va_arg(args, int);
+
+	return (print_number(digit));
 }
+
+/**
+ * print_number - prints an integer into the standard output
+ *
+ * @n: integer to which the recursion will be performed.
+ *
+ * Return: amount of characters printed.
+ */
 
 int print_number(int n)
 {
-    int count = 0;
-    char *minint = "-2147483648";
-    if (n < 0)
-    {
-        if (n < -INT_MAX)
-        {
-            while (minint[count])
-            {
-                _putchar(minint[count]);
-                count++;
-            }
-            return (count--);
-        }
-        else
-        {
-            n = -n;
-        }
-        _putchar('-');
-        count++;
-    }
-    if (n < 10)
-    {
-        _putchar(n + '0');
-        count++;
-    }
-    else
-    {
-        count += print_number(n / 10);
-        _putchar((n % 10) + '0');
-        count++;
-    }
-    return (count);
+	int count = 0;
+	char *minint = "-2147483648";
+
+	if (n < 0)
+	{
+		if (n < -INT_MAX)
+		{
+			while (minint[count])
+			{
+				_putchar(minint[count]);
+				count++;
+			}
+
+			return (count--);
+		}
+
+		else
+		{
+			n = -n;
+		}
+		_putchar('-');
+		count++;
+	}
+
+	if (n < 10)
+	{
+		_putchar(n + '0');
+		count++;
+	}
+
+	else
+	{
+		count += print_number(n / 10);
+		_putchar((n % 10) + '0');
+		count++;
+	}
+	return (count);
 }
